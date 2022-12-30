a14a8eefb11dcff781f448fef9359c4fd0c5f231
3Asterism <842384412@qq.com>
2022-12-30 17:16:17 +0800

    SpringBoot框架 第一次上传

---
+++ src/main/java/com/akisan/universityDataMiddlePlatform/universityDataMiddlePlatformApplication.java
@@ -0,0 +1,11 @@
+package com.akisan.universityDataMiddlePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class universityDataMiddlePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(universityDataMiddlePlatformApplication.class, args);
+    }
+}
