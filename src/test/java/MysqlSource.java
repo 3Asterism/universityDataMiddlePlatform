8c341fb3b953d8c72b1b8375d1d62f23d8a06ea7
3Asterism <842384412@qq.com>
2023-01-05 10:52:00 +0800

    flink测试demo批处理改为流处理

--- src/test/java/MysqlSource.java
+++ src/test/java/MysqlSource.java
@@ -1,9 +1,9 @@
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.types.Row;
 
 public class MysqlSource {
@@ -18,14 +18,16 @@ public class MysqlSource {
                 .setRowTypeInfo(new RowTypeInfo(BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO))
                 .finish();
 
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        DataSource<Row> input1 = env.createInput(input);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<Row> input1 = env.createInput(input);
         input1.map(new MapFunction<Row, String>() {
             @Override
             public String map(Row row) throws Exception {
                 return row.toString();
             }
         }).print();
+
+        env.execute();
         //离线批处理的print(),count(),collect()等都具有execute()的功能。即如果使用了这些就不需要提交execute()了
         //如果是流处理则必须提交execute()
     }
