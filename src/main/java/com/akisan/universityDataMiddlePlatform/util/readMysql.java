ff73104d75e62bf17e764fb084c148bf171fac0e
3Asterism <842384412@qq.com>
2023-01-09 17:46:32 +0800

    数据源提出到util,简化测试demo

--- src/main/java/com/akisan/universityDataMiddlePlatform/util/readMysql.java
+++ src/main/java/com/akisan/universityDataMiddlePlatform/util/readMysql.java
@@ -5,14 +5,16 @@ import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 
 public class readMysql {
-    JDBCInputFormat input = new JDBCInputFormat.JDBCInputFormatBuilder()
-            .setDrivername("com.mysql.cj.jdbc.Driver")
-            .setUsername("root")
-            .setPassword("123456")
-            .setDBUrl("jdbc:mysql://localhost:3306/test_maxwell?serverTimezone=GMT%2b8")
-            .setQuery("select * from test_flink")
-            //设置获取的数据的类型
-            .setRowTypeInfo(new RowTypeInfo(BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO,BasicTypeInfo.INT_TYPE_INFO))
-            .finish();
-
+    public JDBCInputFormat testInput(){
+        JDBCInputFormat input = new JDBCInputFormat.JDBCInputFormatBuilder()
+                .setDrivername("com.mysql.cj.jdbc.Driver")
+                .setUsername("root")
+                .setPassword("123456")
+                .setDBUrl("jdbc:mysql://localhost:3306/test_maxwell?serverTimezone=GMT%2b8")
+                .setQuery("select * from test_flink")
+                //设置获取的数据的类型
+                .setRowTypeInfo(new RowTypeInfo(BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO,BasicTypeInfo.INT_TYPE_INFO))
+                .finish();
+        return input;
+    }
 }
--- src/test/java/MysqlSourceForReduce.java
+++ src/test/java/MysqlSourceForReduce.java
@@ -1,27 +1,17 @@
 import com.akisan.universityDataMiddlePlatform.entity.test_flink;
+import com.akisan.universityDataMiddlePlatform.util.readMysql;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
-import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.types.Row;
 
 public class MysqlSourceForReduce {
     public static void main(String[] args) throws Exception {
-        JDBCInputFormat input = new JDBCInputFormat.JDBCInputFormatBuilder()
-                .setDrivername("com.mysql.cj.jdbc.Driver")
-                .setUsername("root")
-                .setPassword("123456")
-                .setDBUrl("jdbc:mysql://localhost:3306/test_maxwell?serverTimezone=GMT%2b8")
-                .setQuery("select * from test_flink")
-                //设置获取的数据的类型
-                .setRowTypeInfo(new RowTypeInfo(BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO,BasicTypeInfo.INT_TYPE_INFO))
-                .finish();
-
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(8);
-        DataStream<Row> input1 = env.createInput(input);
+        readMysql readMysql = new readMysql();
+        //获取数据源
+        DataStream<Row> input1 = env.createInput(readMysql.testInput());
 
         //transformation
         DataStream<test_flink> test_flinkDataStream = input1.map(new MapFunction<Row, test_flink>() {
@@ -35,11 +25,9 @@ public class MysqlSourceForReduce {
             }
         });
 
+        //reduce
         test_flinkDataStream.keyBy(test_flink::getName).sum("age").print();
 
-//        test_flinkDataStream.print();
         env.execute();
-        //离线批处理的print(),count(),collect()等都具有execute()的功能。即如果使用了这些就不需要提交execute()了
-        //如果是流处理则必须提交execute()
     }
 }
