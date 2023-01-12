e0355fbb1a23d78ee5678683e0d2601c44c9b7ea
3Asterism <842384412@qq.com>
2023-01-12 17:42:03 +0800

    flink 批流一体 aggregate demo提交

---
+++ src/test/java/MysqlSourceForAggregate.java
@@ -0,0 +1,86 @@
+import com.akisan.universityDataMiddlePlatform.entity.test_flink;
+import com.akisan.universityDataMiddlePlatform.util.readAndSinkMysql;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.types.Row;
+
+public class MysqlSourceForAggregate {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(8);
+        readAndSinkMysql readMysql = new readAndSinkMysql();
+        //获取数据源
+        DataStream<Row> input1 = env.createInput(readMysql.testInput());
+
+        //transformation
+        DataStream<test_flink> test_flinkDataStream = input1.map(new MapFunction<Row, test_flink>() {
+            @Override
+            public test_flink map(Row row) throws Exception {
+                return new test_flink(
+                        (Integer) row.getField(0),
+                        (String) row.getField(1),
+                        (Integer) row.getField(2)
+                );
+            }
+        });
+
+        //aggregate
+        SingleOutputStreamOperator<test_flink> aggregate = test_flinkDataStream
+                .keyBy(test_flink::getName)
+                .window(EventTimeSessionWindows.withGap(Time.seconds(3)))
+                //<IN ACC OUT>()
+                .aggregate(new AggregateFunction<test_flink, test_flink, test_flink>() {
+            @Override
+            public test_flink createAccumulator() {
+                test_flink test_flink = new test_flink();
+                test_flink.setAge(0);
+                test_flink.setId(0);
+                test_flink.setName("");
+                return test_flink;
+            }
+
+            @Override
+            public test_flink add(test_flink test_flink, test_flink test_flink2) {
+                test_flink.setAge(test_flink.getAge() + test_flink2.getAge());
+                return test_flink;
+            }
+
+            @Override
+            public test_flink getResult(test_flink test_flink) {
+                test_flink.setAge(test_flink.getAge()/2);
+                return test_flink;
+            }
+
+            @Override
+            public test_flink merge(test_flink test_flink, test_flink acc1) {
+                return null;
+            }
+        });
+
+        //Ready to sink
+        DataStream<Row> resultSink = aggregate.map(new MapFunction<test_flink, Row>() {
+            @Override
+            public Row map(test_flink test_flink) throws Exception {
+                Row row = new Row(3);
+                row.setField(0, test_flink.getId());
+                row.setField(1, test_flink.getName());
+                row.setField(2, test_flink.getAge());
+                return row;
+            }
+        });
+
+        readAndSinkMysql sinkMysql = new readAndSinkMysql();
+
+        String query = "INSERT INTO test_maxwell.test_flinksink (id, name, age) VALUES (?, ?, ?)";
+
+        //Sink
+        resultSink.writeUsingOutputFormat(sinkMysql.testOutput(query));
+
+        env.execute();
+    }
+}
--- src/test/java/MysqlSourceForMap.java
+++ src/test/java/MysqlSourceForMap.java
@@ -1,14 +1,10 @@
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.types.Row;
-import org.apache.flink.util.Collector;
 
 public class MysqlSourceForMap {
     public static void main(String[] args) throws Exception {
